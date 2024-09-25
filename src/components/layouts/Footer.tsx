19b60a3f7d058bb4ac2b4bae2161c1bc93656e9d
T.S. Chief <fullstack.star0123@gmail.com>
2024-09-25 19:56:16 +0900

    Update Footer.tsx

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -8,9 +8,6 @@ const Footer = () => {
         className="font-normal text-inherit text-center flex items-center justify-center"
       >
         &copy;{" "}
-        <a className="mx-1" href="https:johnmwendwa.vercel.app" target="_blank">
-          John Mwendwa{" "}
-        </a>
         {new Date().getFullYear()}
       </Typography>
     </footer>
